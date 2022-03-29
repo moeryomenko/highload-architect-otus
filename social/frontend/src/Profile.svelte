f44d853fce382ad594ef68ff1746fbd89730807d
Maxim Eryomenko <moeryomenko@gmail.com>
2022-03-29 19:01:38 +0300

    feat(front): add profile submit page
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ social/frontend/src/Profile.svelte
@@ -0,0 +1,54 @@
+<script>
+</script>
+
+<div class="field">
+  <label class="label">First name</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Text input">
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Last name</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Text input">
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Age</label>
+  <div class="control">
+    <input class="input" type="number">
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Gender</label>
+  <p class="control has-icons-left">
+    <span class="select">
+      <select>
+        <option>Male</option>
+        <option>Female</option>
+      </select>
+    </span>
+    <span class="icon is-small is-left">
+      <i class="fas fa-globe"></i>
+    </span>
+  </p>
+</div>
+
+<div class="field">
+  <label class="label">City</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Krasnodar">
+  </div>
+</div>
+
+<div class="field is-grouped">
+  <div class="control">
+    <button class="button is-link">Submit</button>
+  </div>
+  <div class="control">
+    <button class="button is-link is-light">Cancel</button>
+  </div>
+</div>
