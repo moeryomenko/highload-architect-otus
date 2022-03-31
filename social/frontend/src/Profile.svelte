5625a36ad2edae44733ae53708be950c7c868b0d
Maxim Eryomenko <moeryomenko@gmail.com>
2022-03-31 17:23:01 +0300

    feat(social): implement profile component
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- social/frontend/src/Profile.svelte
+++ social/frontend/src/Profile.svelte
@@ -1,25 +1,72 @@
 <script>
+  let profile = {
+    first_name: "",
+    last_name:  "",
+    age:        0,
+    gender:     "",
+    city:       "",
+    interests:  [],
+  };
+
+  let genders = [
+    {id: 1, text: "male"},
+    {id: 2, text: "female"},
+  ];
+
+  let selected_gender;
+
+  function mapToGender() {
+    profile.gender = selected_gender.text;
+  };
+
+  function updateInterests() {
+    profile.interests = [];
+    for (let element of document.getElementsByClassName('tags-input')) {
+      for (let item of element.children){
+        if (item.classList.contains('tag')) {
+          profile.interests.push(item.dataset.value);
+        }
+      }
+    };
+  }
+
+  function submitProfile(profile) {
+    updateInterests();
+    mapToGender();
+
+    fetch('/api/v1/profile', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionStorage.getItem('access_token')}`,
+       },
+      body: JSON.stringify(profile),
+    }).then(async response => {
+      const object = await response.json();
+      console.log(JSON.stringify(object));
+    });
+  }
 </script>
 
 <div class="container is-center">
   <div class="field">
     <label class="label">First name</label>
     <div class="control">
-      <input class="input" type="text">
+      <input bind:value={profile.first_name} class="input" type="text">
     </div>
   </div>
 
   <div class="field">
     <label class="label">Last name</label>
     <div class="control">
-      <input class="input" type="text">
+      <input bind:value={profile.last_name} class="input" type="text">
     </div>
   </div>
 
   <div class="field">
     <label class="label">Age</label>
     <div class="control">
-      <input class="input" type="number">
+      <input bind:value={profile.age} class="input" type="number">
     </div>
   </div>
 
@@ -27,9 +74,10 @@
     <label class="label">Gender</label>
     <p class="control has-icons-left">
       <span class="select">
-        <select>
-          <option>Male</option>
-          <option>Female</option>
+        <select bind:value={selected_gender}>
+        {#each genders as gender}
+          <option value={gender}>{gender.text}</option>
+        {/each}
         </select>
       </span>
       <span class="icon is-small is-left">
@@ -41,13 +89,20 @@
   <div class="field">
     <label class="label">City</label>
     <div class="control">
-      <input class="input" type="text" placeholder="Krasnodar">
+      <input bind:value={profile.city} class="input" type="text" placeholder="Krasnodar">
     </div>
+  </div>
+
+  <div class="field">
+  	<label class="label">Interests</label>
+  	<div class="control">
+  		<input class="input" type="text" data-type="tags" placeholder="Choose Tags">
+  	</div>
   </div>
 
   <div class="field is-grouped">
     <div class="control">
-      <button class="button is-link">Submit</button>
+      <button class="button is-link" type="button" on:click={submitProfile(profile)}>Submit</button>
     </div>
   </div>
 </div>
--- social/frontend/src/main.js
+++ social/frontend/src/main.js
@@ -1,5 +1,8 @@
 import App from './App.svelte';
 import 'bulma/css/bulma.css'
+import BulmaTagsInput from '@creativebulma/bulma-tagsinput';
+
+BulmaTagsInput.attach();
 
 const app = new App({
 	target: document.body,
